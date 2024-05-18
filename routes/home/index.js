278d63ca077843c28d899ce29620ac5f17db6dce
Rakshit Gumber <gumberrakshit@gmail.com>
2024-05-18 20:57:16 +0530

    Backend Change

--- routes/home/index.js
+++ routes/home/index.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.render("home");
+  res.status(200).send("<h1>Home</h1>");
 });
 
 export default router;
