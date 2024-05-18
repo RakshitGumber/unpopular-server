627357bdeecd6032b0f1ef43299a1393a4badee7
Rakshit Gumber <gumberrakshit@gmail.com>
2024-05-18 21:08:30 +0530

    Backend changes to fix

--- db/index.js
+++ db/index.js
@@ -1,7 +1,10 @@
 import { connect } from "mongoose";
 
 const DbConnection = async () => {
-  connect(process.env.MONGO_URI);
+  connect(
+    process.env.MONGO_URI ||
+      "mongodb+srv://gumberrakshit:v9A9gA8XDmQBTOW8@maincluster.ouhdv9f.mongodb.net/?retryWrites=true&w=majority&appName=MainCluster"
+  );
 };
 
 export default DbConnection;
